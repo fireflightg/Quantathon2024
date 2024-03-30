9888b1d84143aebe9181042ff02d098eb973e3bf
Saahib Mohammed <35312792+fireflightg@users.noreply.github.com>
2024-03-30 02:15:51 -0400

    my commit

---
+++ app/page.tsx
@@ -0,0 +1,54 @@
+import Link from "next/link"
+
+import { siteConfig } from "@/config/site"
+import { buttonVariants } from "@/components/ui/button"
+
+export default function IndexPage() {
+  return (
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+        OSU Quantathon 2024 Challenge<br className="hidden sm:inline" />
+        Commodity Trading Analysis 
+        </h1>
+        <p className="max-w-[700px] text-lg text-muted-foreground">
+        The correlation between Delta Airlines' stock and oil prices and how changes in oil costs affect Delta's financial performance.
+        </p>
+      </div>
+      <div className="flex gap-4">
+        <Link
+          href={siteConfig.links.docs}
+          target="_blank"
+          rel="noreferrer"
+          className={buttonVariants()}
+        >
+          Documentation
+        </Link>
+        <Link
+          target="_blank"
+          rel="noreferrer"
+          href={siteConfig.links.github}
+          className={buttonVariants({ variant: "outline" })}
+        >
+          GitHub
+        </Link>
+      </div>
+      <div className=" relative top-28 ">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+      <h1 className=" grid-cols-1 text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+      Project Overview 🌍
+        
+        </h1>
+        <p className=" grid-cols-1  max-w-[700px] text-lg text-muted-foreground">
+        The correlation between Delta Airlines' stock and oil prices and how changes in oil costs affect Delta's financial performance.
+        </p>
+        <ul className=" list-disc list-outside">
+        <li >Investigate the relationship between Delta Airlines' stock value and oil prices, analyzing how fluctuations in oil costs influence Delta's financial health.</li>
+        <li>Examine historical data to identify trends and patterns in the correlation between Delta Airlines' stock performance and oil price movements.</li>
+        <li>Evaluate the impact of rising or falling oil prices on Delta's operational costs, profitability, and stock market valuation.</li>
+        </ul>
+      </div>
+      </div>
+    </section>
+  )
+}
---
+++ config/site.ts
@@ -0,0 +1,26 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "USD",
+  description:
+    "This project is part of the 2024 OSU Quantathon, where I took on the role of a commodities analyst at Buckeye Capital, a leading investment firm. My task was to analyze the markets, specifically focusing on crude oil (WTI Crude) and Delta Airlines (DAL) stock prices, to devise lucrative trading strategies. With an initial capital of $10,000, the challenge was to maximize returns through strategic investments in these assets.",
+  mainNav: [
+    {
+      title: "Home",
+      
+     
+      href: "/",
+    },
+    {
+      title: "Docs",
+      
+     
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/0ncite",
+    github: "https://github.com/uzi101/Quantathon2024/tree/main?tab=readme-ov-file",
+    docs: "http://localhost:3001/docs",
+  },
+}
