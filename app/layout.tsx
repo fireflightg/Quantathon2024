e83d1850377dec1d908a7cc534d83596ed307bb8
Saahib Mohammed <fireflightwork@gmail.com>
2024-03-31 17:01:55 -0400

    yuee

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,24 @@ import "@/styles/globals.css"
 import { Metadata } from "next"
 
 import { siteConfig } from "@/config/site"
-import { fontSans } from "@/lib/fonts"
+
 import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
+
+import { JetBrains_Mono as FontMono, Inter as FontSans } from "next/font/google"
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+const fontMono = FontMono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
+
 
 export const metadata: Metadata = {
   title: {
